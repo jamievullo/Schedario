72315dc556365f5cfb3749af4aa665b664adcc11
jamievullo <jvullo@protonmail.com>
2019-10-05 09:10:47 -0400

    Added stylesheets and scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   get 'welcome/index'
   
   root 'welcome#index'
+  authenticated :user do 
+    root 'docs#index', as: 'authenticated_root'
+  end
   
   resources :docs
 end
